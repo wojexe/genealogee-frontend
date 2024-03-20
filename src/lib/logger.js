fa29454fec1052e2061b9171671544e9db363f40
Wojciech Sierotowicz <wojexe@gmail.com>
2024-03-20 01:00:20 +0100

    Add Pino logger

---
+++ src/lib/logger.js
@@ -0,0 +1,17 @@
+import pino from "pino";
+import { dev } from "$app/environment";
+
+/** @type import('pino').LoggerOptions */
+const options = {};
+
+if (dev) {
+  options.level = "debug";
+  options.transport = {
+    target: "pino-pretty",
+    options: {
+      colorize: true,
+    },
+  };
+}
+
+export const logger = pino(options);
