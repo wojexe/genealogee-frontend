ed27705b2de10d9f3cfe5ed6a2b339b94d91f9a3
Wojciech Sierotowicz <wojexe@gmail.com>
2024-05-01 19:42:12 +0200

    Add conditional dialog for edit/delete person

--- src/lib/components/ui/tree/tree-person.svelte
+++ src/lib/components/ui/tree/tree-person.svelte
@@ -2,6 +2,7 @@
   import type { Tree } from "$lib/genealogee";
   import { Card, Content } from "$lib/components/ui/card";
   import * as ContextMenu from "$lib/components/ui/context-menu/index.js";
+  import * as Dialog from "$lib/components/ui/dialog";
 
   type Props = {
     personID: string;
@@ -16,28 +17,51 @@
 
   // TODO: error handling
   let person = $derived(tree.people.get(personID)!);
+
+  type DialogContentState = "edit" | "delete";
+  let dialogContent: DialogContentState = $state("edit");
 </script>
 
-<ContextMenu.Root>
-  <ContextMenu.Trigger>
-    <Card class="min-w-48 max-w-82 break-words flex flex-col">
-      <Content>
-        {person.givenNames} <b>{person.familyName}-{person.birthName}</b>
-      </Content>
-    </Card>
-  </ContextMenu.Trigger>
+<Dialog.Root>
+  <ContextMenu.Root>
+    <ContextMenu.Trigger>
+      <Card class="min-w-48 max-w-82 break-words flex flex-col">
+        <Content>
+          {person.givenNames} <b>{person.familyName}-{person.birthName}</b>
+        </Content>
+      </Card>
+    </ContextMenu.Trigger>
 
-  <!-- TODO: make editable and deletable -->
-  <ContextMenu.Content>
-    <ContextMenu.Item>
-      <Pencil />
-      Edit
-    </ContextMenu.Item>
-    <ContextMenu.Item
-      class="data-[highlighted]:bg-destructive data-[highlighted]:text-destructive-foreground"
-    >
-      <Trash />
-      Delete
-    </ContextMenu.Item>
-  </ContextMenu.Content>
-</ContextMenu.Root>
+    <ContextMenu.Content>
+      <Dialog.Trigger
+        class="block w-[100%]"
+        onclick={() => (dialogContent = "edit")}
+      >
+        <ContextMenu.Item>
+          <Pencil />
+          Edit
+        </ContextMenu.Item>
+      </Dialog.Trigger>
+
+      <Dialog.Trigger
+        class="block w-[100%]"
+        onclick={() => (dialogContent = "delete")}
+      >
+        <ContextMenu.Item
+          class="data-[highlighted]:bg-destructive data-[highlighted]:text-destructive-foreground"
+        >
+          <Trash />
+          Delete
+        </ContextMenu.Item>
+      </Dialog.Trigger>
+    </ContextMenu.Content>
+  </ContextMenu.Root>
+
+  <Dialog.Content>
+    {#if dialogContent == "edit"}
+      Edit Dialog
+    {:else if dialogContent == "delete"}
+      Delete Dialog
+    {/if}
+  </Dialog.Content>
+</Dialog.Root>
