cc90636bc4bc900b13835a329d30b2f781ee09ae
Wojciech Sierotowicz <wojexe@gmail.com>
2024-05-01 18:45:46 +0200

    Center the tree on mount

--- src/lib/components/ui/tree/tree-root.svelte
+++ src/lib/components/ui/tree/tree-root.svelte
@@ -9,12 +9,23 @@ type Props = {
 <script lang="ts">
 import Family from "./tree-family.svelte";
 import { AddFirstPerson } from ".";
+import { onMount } from "svelte";
 
 const { tree }: Props = $props();
+
+let container: HTMLDivElement;
+
+onMount(()=> {
+  container.scrollTo({
+    top: Math.max(0, container.scrollHeight - container.clientHeight),
+    left: Math.max(0, container.scrollWidth / 2 - container.clientWidth / 2),
+    behavior: "instant",
+  });
+})
 </script>
 
 <div class="h-fit w-fit rounded-3xl overflow-hidden shadow-sm">
-  <div class="flex place-content-center h-fit w-[90vw] overflow-auto rounded-3xl border border-red px-8 py-10">
+  <div bind:this={container} class="flex h-fit w-[90vw] overflow-auto rounded-3xl border border-red px-8 py-10">
     {#if tree.people.size === 0}
       <AddFirstPerson tree={tree} />
     {:else}
