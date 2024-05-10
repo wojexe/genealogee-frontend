0f0b2a4e685f9e782cb440f1ea80a9f68f2ed352
Wojciech Sierotowicz <wojexe@gmail.com>
2024-05-11 01:52:32 +0200

    Vercel?!

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,9 @@
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
-import adapter from "svelte-adapter-bun";
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
-    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
     alias: {
       $paraglide: "./src/paraglide",
