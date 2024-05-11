792480c55756eff75424c0af93503c7ed0aede27
Wojciech Sierotowicz <wojexe@gmail.com>
2024-05-11 02:01:15 +0200

    Vite conditional https on dev server

--- vite.config.js
+++ vite.config.js
@@ -3,22 +3,32 @@ import { paraglide } from "@inlang/paraglide-js-adapter-sveltekit/vite";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vitest/config";
 
-export default defineConfig({
-  plugins: [
-    sveltekit(),
-    paraglide({
-      project: "./project.inlang",
-      outdir: "./src/paraglide",
-    }),
-  ],
-  server: {
-    https: {
-      key: await readFile("./certs/key.pem"),
-      cert: await readFile("./certs/cert.pem"),
+export default defineConfig(async ({ mode }) => {
+  let extraOptions = {};
+
+  if (mode === "development") {
+    extraOptions = {
+      ...extraOptions,
+      server: {
+        https: {
+          key: await readFile("./certs/key.pem"),
+          cert: await readFile("./certs/cert.pem"),
+        },
+        proxy: {},
+      },
+    };
+  }
+
+  return {
+    plugins: [
+      sveltekit(),
+      paraglide({
+        project: "./project.inlang",
+        outdir: "./src/paraglide",
+      }),
+    ],
+    test: {
+      include: ["src/**/*.{test,spec}.{js,ts}"],
     },
-    proxy: {},
-  },
-  test: {
-    include: ["src/**/*.{test,spec}.{js,ts}"],
-  },
+  };
 });
